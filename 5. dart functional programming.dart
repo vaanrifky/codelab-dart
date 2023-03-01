d59220aa17018ab25870601e880b127f108c9873
vaanrifky <106819876+vaanrifky@users.noreply.github.com>
2023-03-01 13:08:17 +0700

    Add files via upload

---
+++ 5. dart functional programming.dart
@@ -0,0 +1,9 @@
+// Dart for functional programming
+String scream(int length) => "A${'a' * length}h!";
+
+main() {
+  final values = [1, 2, 3, 5, 10, 50];
+  for (var length in values) {
+    print(scream(length));
+  }
+}
